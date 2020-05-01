54a8f24d37d3c4f52af5e9579c570aeff200a81b
Kearny <alexandre.liscia@gmail.com>
2020-05-01 11:09:26 +0200

    Adds a blinking light script.

--- Assets/Scripts/Com.Kearny.Shooter/GameMechanics/SunManager.cs
+++ Assets/Scripts/Com.Kearny.Shooter/GameMechanics/SunManager.cs
@@ -4,13 +4,19 @@ namespace Com.Kearny.Shooter.GameMechanics
 {
     public class SunManager : MonoBehaviour
     {
+        [Header("Sun")] [Tooltip("Enable the sun rotation")]
+        public bool isRotating;
+
         [Tooltip("The speed at which sun is rotating (0.1 equals a complete rotation in 1h)")]
         public float rotationSpeed;
 
         // Update is called once per frame
         private void Update()
         {
-            transform.Rotate(Vector3.right * (rotationSpeed * Time.deltaTime));
+            if (isRotating)
+            {
+                transform.Rotate(Vector3.right * (rotationSpeed * Time.deltaTime));
+            }
         }
     }
 }
