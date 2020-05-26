a8d2c73326fb5859b4b32df6c54a90e4a512c8e4
Kearny <alexandre.liscia@gmail.com>
2020-05-26 13:30:38 +0200

    Network base code for ghost generated

---
+++ Assets/Scripts/Com.Kearny.Shooter/Networking/MovableCubeComponent.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using Unity.NetCode;
+
+namespace Com.Kearny.Shooter.Networking
+{
+    [GenerateAuthoringComponent]
+    public struct MovableCubeComponent : IComponentData
+    {
+        [GhostDefaultField] public int PlayerId;
+    }
+}
