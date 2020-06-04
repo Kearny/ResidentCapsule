d19faba463fcd86d6d3a62e57a2d659e8a1e76a7
CarbonDatedPotato <kyuushi_kotsuki@hotmail.fr>
2020-06-04 19:56:19 +0200

    Map

--- Assets/Scripts/Com.Kearny.Shooter/Enemy/Enemy.cs
+++ Assets/Scripts/Com.Kearny.Shooter/Enemy/Enemy.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using Com.Kearny.Shooter.GameMechanics;
 using Unity.Burst;
 using UnityEngine;
 using UnityEngine.AI;
-
 namespace Com.Kearny.Shooter.Enemy
 {
     [BurstCompile]
@@ -20,15 +20,15 @@ namespace Com.Kearny.Shooter.Enemy
         private State _currentState;
 
         public GameObject deathEffect;
-        private ParticleSystem _deathEffectParticleSystem;
 
+        private ParticleSystem _deathEffectParticleSystem;
         private NavMeshAgent _pathFinder;
         private Transform _target;
         private LivingEntity _targetEntity;
-
         private const float AttackDistanceThreshold = 1.5f;
         private const float TimeBetweenAttacks = 1;
         private const float Damage = 1;
+
         private float _nextAttackTime;
         private float _myCollisionRadius;
         private float _targetCollisionRadius;
@@ -37,33 +37,55 @@ namespace Com.Kearny.Shooter.Enemy
         protected override void Start()
         {
             base.Start();
-
             _deathEffectParticleSystem = deathEffect.GetComponent<ParticleSystem>();
-
             _pathFinder = GetComponent<NavMeshAgent>();
-
             if (!GameObject.FindGameObjectWithTag("Player")) return;
-
             _currentState = State.Chasing;
-            _hasTarget = true;
-
-            _target = GameObject.FindGameObjectWithTag("Player").transform;
+            AcquireTarget();
             _targetEntity = _target.GetComponent<LivingEntity>();
+            Debug.Log(_targetEntity.name);
             _targetEntity.OnDeath += OnTargetDeath;
-
             _myCollisionRadius = GetComponent<CapsuleCollider>().radius;
             _targetCollisionRadius = _target.GetComponent<CapsuleCollider>().radius;
-
             StartCoroutine(UpdatePath());
+        }
+
+        private void AcquireTarget()
+        {
+            var targetTransforms = new List<Transform> { GameObject.FindGameObjectWithTag("Player").transform };
+            foreach (var targetObject in GameObject.FindGameObjectsWithTag("Target"))
+            {
+                targetTransforms.Add(targetObject.transform);
+            }
+            _target = FindClosestTarget(targetTransforms);
+            _hasTarget = _target != null;
+        }
+
+        private Transform FindClosestTarget(IEnumerable<Transform> targetTransforms)
+        {
+            Transform closest = null;
+            var distance = Mathf.Infinity;
+            Vector3 position = transform.position;
+            foreach (var targetTransform in targetTransforms)
+            {
+                var distanceDiff = targetTransform.position - position;
+                var currentDistance = distanceDiff.sqrMagnitude;
+                if (currentDistance < distance)
+                {
+                    closest = targetTransform;
+                    distance = currentDistance;
+                }
+            }
+            return closest;
         }
 
         private void Update()
         {
+            AcquireTarget();
             if (!_hasTarget) return;
+            Debug.Log(_targetEntity.name);
             if (!(Time.time > _nextAttackTime)) return;
-
             var squareDistanceToTarget = (_target.position - transform.position).sqrMagnitude;
-
             // If can Attack
             if (squareDistanceToTarget < Mathf.Pow(
                 AttackDistanceThreshold + _myCollisionRadius + _targetCollisionRadius,
@@ -74,7 +96,6 @@ namespace Com.Kearny.Shooter.Enemy
                 StartCoroutine(Attack());
             }
         }
-
         public override void TakeHit(float damage, Vector3 hitLocation, Vector3 hitDirection)
         {
             if (damage >= health)
@@ -84,54 +105,42 @@ namespace Com.Kearny.Shooter.Enemy
                     _deathEffectParticleSystem.main.duration
                 );
             }
-
             base.TakeHit(damage, hitLocation, hitDirection);
         }
-
         private void OnTargetDeath()
         {
             _hasTarget = false;
             _currentState = State.Idle;
         }
-
         private IEnumerator Attack()
         {
             _currentState = State.Attacking;
-
             var transformPosition = transform.position;
             var originalPosition = transformPosition;
-
             var targetPosition = _target.position;
             var directionToTarget = (targetPosition - transformPosition).normalized;
             var attackPosition = targetPosition - directionToTarget;
-
             const float attackSpeed = 3;
-            float percent = 0; 
-
+            float percent = 0;
             var hasAppliedDamage = false;
-
             while (percent <= 1)
             {
                 if (percent <= .8f && !hasAppliedDamage)
                 {
                     hasAppliedDamage = true;
+                    Debug.Log("Le bot tape sur : " + _targetEntity.name);
                     _targetEntity.TakeDamage(Damage);
                 }
-
                 percent += Time.deltaTime * attackSpeed;
                 var interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
                 transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
-
                 yield return null;
             }
-
             _currentState = State.Chasing;
         }
-
         private IEnumerator UpdatePath()
         {
             const float refreshRate = 0.5f;
-
             while (_hasTarget)
             {
                 if (!isDead && _currentState == State.Chasing)
@@ -143,7 +152,6 @@ namespace Com.Kearny.Shooter.Enemy
                         (_myCollisionRadius + _targetCollisionRadius + AttackDistanceThreshold / 2);
                     _pathFinder.SetDestination(targetedPosition);
                 }
-
                 yield return new WaitForSeconds(refreshRate);
             }
         }
--- Assets/Scripts/Com.Kearny.Shooter/GameMechanics/Door.cs
+++ Assets/Scripts/Com.Kearny.Shooter/GameMechanics/Door.cs
@@ -1,79 +1,62 @@
 ﻿using UnityEngine;
+
 namespace Com.Kearny.Shooter.GameMechanics
 {
-    public class DoorManager : MonoBehaviour
+    public class Door : LivingEntity
     {
         private enum DoorState
         {
-            OPENED,
-            CLOSED,
-            BROKEN
+            Opened,
+            Closed
         }
-
         public float speed = 1;
         public float openDistance = 5;
-
-        private DoorState _doorState = DoorState.CLOSED;
-        private Vector3 _worldOpenPosition;
+        private DoorState _doorState = DoorState.Closed;
         private Vector3 _openPosition;
         private Vector3 _closedPosition;
         private Transform _playerTransform;
-
         // Start is called before the first frame update
-        void Start()
+        protected override void Start()
         {
-            _closedPosition = transform.position;
-            _openPosition = new Vector3((transform.position.x - transform.localScale.x), transform.position.y, transform.position.z);
+            base.Start();
+            var transform1 = transform;
+            var position = transform1.position;
+            _closedPosition = position;
+            _openPosition = new Vector3((position.x - transform1.localScale.x), position.y, position.z);
+            _playerTransform = GameObject.FindGameObjectsWithTag("Player")[0].transform;
         }
-
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
-            var players = GameObject.FindGameObjectsWithTag("Player");
-            _playerTransform = players[0].transform;
-            
-            if (_doorState == DoorState.BROKEN)
-            {
-                return;
-            }
-
-            if (PlayerInReach() && _doorState == DoorState.CLOSED)
+            if (PlayerInReach() && _doorState == DoorState.Closed)
             {
                 Open();
             }
-            else if (!PlayerInReach() && _doorState != DoorState.CLOSED)
+            else if (!PlayerInReach() && _doorState != DoorState.Closed)
             {
-                Debug.Log(PlayerInReach());
                 Close();
             }
         }
-
         private bool PlayerInReach()
         {
             var distance = Vector3.Distance(_closedPosition, _playerTransform.position);
-            Debug.Log(distance < openDistance);
-
             return distance < openDistance;
         }
-
         private void Open()
         {
-            Debug.Log("Opening the door");
             while (transform.position != _openPosition)
             {
                 transform.position = Vector3.MoveTowards(transform.position, _openPosition, Time.deltaTime * speed);
             }
-            _doorState = DoorState.OPENED;
+            _doorState = DoorState.Opened;
         }
-
         private void Close()
         {
-            Debug.Log("Closing the door");
             while (transform.position != _closedPosition)
             {
                 transform.position = Vector3.MoveTowards(transform.position, _closedPosition, Time.deltaTime * speed);
             }
-            _doorState = DoorState.CLOSED;
+            _doorState = DoorState.Closed;
         }
     }
 }
